9bbbbc4ef025ffed87c1ab94dd403a1e84ed1b80
HelloWorld <63961011+loosaca@users.noreply.github.com>
2022-05-26 10:52:30 +0800

    Update virtual_destruction.cpp

--- virtual_destruction.cpp
+++ virtual_destruction.cpp
@@ -14,3 +14,11 @@ public:
     }
     ...
 };
+int main()
+{
+    Student *s = new Student("Simpson", CSE, 3.8);
+    s->add_course("COMP1021");
+    s->add_course("COMP2012");
+    UPerson *p = s;
+    delete p; // Actually call Student’s destructor
+}
