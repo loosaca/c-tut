e3da840f5b8394216cfdaec60e8ce893542050ef
HelloWorld <63961011+loosaca@users.noreply.github.com>
2022-05-26 10:41:46 +0800

    Delete int main() { Student* s = new Student("Simpson", CSE, 3.8); s->add_course("COMP1021"); s->add_course("COMP2012"); UPerson* p = s; delete p; directory

--- int main() { Student* s = new Student("Simpson", CSE, 3.8); s->add_course("COMP1021"); s->add_course("COMP2012"); UPerson* p = s; delete p;/destruction_example2.cpp
+++
@@ -1,8 +0,0 @@
-int main()
-{
-    Student *s = new Student("Simpson", CSE, 3.8);
-    s->add_course("COMP1021");
-    s->add_course("COMP2012");
-    UPerson *p = s;
-    delete p; // Can we call UPerson’s destructor on a Student?
-}
